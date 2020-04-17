b3e67135e4a93d199064de8909fb35df2f07a7a9
HimanshuVarun <himanshuvarun9756@gmail.com>
2020-04-17 16:06:52 +0530

    first commit

---
+++ src/test/java/sum2/sum2/Add.java
@@ -0,0 +1,20 @@
+package sum2.sum2;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Add 
+{
+	@BeforeTest
+	public void aa()
+	{
+		System.out.println("helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
+	}
+	@Test
+	public void test()
+	{
+		System.out.println("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+		
+	}
+
+}
